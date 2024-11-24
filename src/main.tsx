9834f1f35209048e24b3f79a258314b625695b80
M. Aji Perdana <ajicooljazz38@gmail.com>
2024-11-24 15:03:25 +0700

    feat: init react memoize ✅

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")!, {
+  identifierPrefix: "react-",
+}).render(
   <StrictMode>
     <App />
   </StrictMode>
